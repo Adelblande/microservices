9be3ce8438808b6d552c67381fb634503aa03a91
Adelblande <adelblande@gmail.com>
2023-10-02 16:23:39 -0300

    Config Apollo federation

--- classroom/src/http/graphql/resolvers/students.resolver.ts
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -4,7 +4,7 @@ import { Student } from '../models/student';
 import { EnrollmentsService } from 'src/services/enrollments.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
-import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
+// import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
 
 @Resolver(() => Student)
 export class StudentsResolver {
@@ -13,16 +13,17 @@ export class StudentsResolver {
     private enrollmentsService: EnrollmentsService,
   ) {}
 
+  @UseGuards(AuthorizationGuard)
   @Query(() => [Student])
   findAllStudents() {
     return this.studentsService.findAll();
   }
 
-  @UseGuards(AuthorizationGuard)
-  @Query(() => Student)
-  me(@CurrentUser() user: AuthUser) {
-    return this.studentsService.getStudentByAuthUserId(user.sub);
-  }
+  // @UseGuards(AuthorizationGuard)
+  // @Query(() => Student)
+  // me(@CurrentUser() user: AuthUser) {
+  //   return this.studentsService.getStudentByAuthUserId(user.sub);
+  // }
 
   @ResolveField()
   enrollments(@Parent() student: Student) {

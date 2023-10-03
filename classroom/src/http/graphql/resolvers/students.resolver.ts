1bc399c131b49e421f95ffe61a86340b011d5129
Adelblande <adelblande@gmail.com>
2023-10-03 00:16:02 -0300

    Completed back-end

--- classroom/src/http/graphql/resolvers/students.resolver.ts
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -4,7 +4,7 @@ import { Student } from '../models/student';
 import { EnrollmentsService } from 'src/services/enrollments.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
-// import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
+import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
 
 @Resolver(() => Student)
 export class StudentsResolver {
@@ -19,11 +19,11 @@ export class StudentsResolver {
     return this.studentsService.findAll();
   }
 
-  // @UseGuards(AuthorizationGuard)
-  // @Query(() => Student)
-  // me(@CurrentUser() user: AuthUser) {
-  //   return this.studentsService.getStudentByAuthUserId(user.sub);
-  // }
+  @UseGuards(AuthorizationGuard)
+  @Query(() => Student)
+  me(@CurrentUser() user: AuthUser) {
+    return this.studentsService.getStudentByAuthUserId(user.sub);
+  }
 
   @ResolveField()
   enrollments(@Parent() student: Student) {
